b7be78f0137f0346cf7f23655a55cfb19f027983
renato.guimaraes <renato.guimaraes@novitatus.com>
2017-03-15 10:04:40 -0300

    Initial import, api exploration.

---
+++ ethereum/src/main/java/ethereum/Main.java
@@ -0,0 +1,38 @@
+package ethereum;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+
+public class Main
+{
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException, IOException
+    {
+        Web3j web3 = Web3j.build(new HttpService());
+
+        web3.transactionObservable().subscribe(tx -> {
+            System.out.println("------------------");
+            System.out.println("Block hash " + tx.getBlockHash());
+            System.out.println("Block number " + tx.getBlockNumber());
+            System.out.println("Creates " + tx.getCreates());
+            System.out.println("From " + tx.getFrom());
+            System.out.println("Gas " + tx.getGas());
+            System.out.println("Gas price " + tx.getGasPrice());
+            System.out.println("Hash " + tx.getHash());
+            System.out.println("Input " + tx.getInput());
+            System.out.println("Nonce " + tx.getNonce());
+            System.out.println("Public key " + tx.getPublicKey());
+            System.out.println("R " + tx.getR());
+            System.out.println("Raw " + tx.getRaw());
+            System.out.println("S " + tx.getS());
+            System.out.println("To " + tx.getTo());
+            System.out.println("Transaction index " + tx.getTransactionIndex());
+            System.out.println("Value " + tx.getValueRaw());
+        });
+
+    }
+
+}
